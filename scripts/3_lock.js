5c85676da61e629c61de2177832ac90ed169e03c
heipi <haiheipijuan@gmail.com>
2023-10-14 23:43:55 +0800

    update lock script

--- scripts/3_lock.js
+++ scripts/3_lock.js
@@ -9,19 +9,17 @@ const IERC20_SOURCE = "@openzeppelin/contracts/token/ERC20/IERC20.sol:IERC20";
 
 // params for goerli
 // pubkey should not have the leading 04 prefix
-let testPri1 = "0xed3f195449409dc1d22f0119da7678200710a447ff75baa06bfc42829d02d28c"
-let testPub1 = "0xb548c2a036db2b5cdeb9501ff6c73e4653b691e3365e345bdddeb20145fa2f50f8a527550211b4b0664127a36f370d91c2adc33b14423327097f411c4c68ee96"
-let addr1 = "0x1267397FB5BF6F6DCC3D18D673616D512DBCD8F0"
+let seqPub1 = "0xb548c2a036db2b5cdeb9501ff6c73e4653b691e3365e345bdddeb20145fa2f50f8a527550211b4b0664127a36f370d91c2adc33b14423327097f411c4c68ee96"
+let seqAddr1 = "0x1267397fb5BF6f6Dcc3d18d673616D512dbcd8F0"
 
-let testPri2 = "0x87f4f773ff72685f05494c4d2f4b484d9ac958f9d010b9abda174e9ce1266e05"
-let testPub2 = "0xde61a8a5e89510a7dcbcf9ec79aa4ae502341f001b53c0fdb08f4d16adff9c1848b7301f8f616a45a3aad950a68235e41a122fd8f213b1b7f98f661c91acefc2"
-let addr2 = "0x57114948f79fa1c2be29b814ff19957c1fe8f64a"
+let seqPub2 = "0xa95fde90a1a2ff755ed81ecf8f9a4761ca2e2e1bd017ba202f6d4e222d591f9b8a98718dab19521c6594eeb41ed2707a3e3861cbf5f730de80281cbcf846f3b5"
+let seqAddr2 = "0x432e8fB4C4BD8Ce19DC8c05d6915Ab68C336B147"
 
-let testPri4 = "0x35adc09de5c4ba56e824695ab03be7d5d91cead985a685d3ca44920e69d391ca"
-let testPub4 = "0x51b5cfe5551bafd4039d7804bb660194f386ae6e171f90ce96be77870fc8f3dcca5a7ddfd9fd897f27999a443d2066e90281eb1f6915b41478103527884a4be7"
-let addr4 = "0x1cc73d42a0a081F442daDf40B89A8A86ECc69b55"
+let seqPub3 = "0x94f65bb41bffc7a86abbbe21a9edc68f2fa005f5c366ae130cbca779173e0f46644936d4bd56949d627320e8b66a8ed084d178d4b494172e3a5d8d33855dbde3"
+let seqAddr3 = "0xFe08EE83B1f01d6d7C6eff3c8C84FA6Fe02fca17"
 
-let signerPubkey = "0x2d905ff3831bb3e3bec873544cd8da2796b5a3a699f0e8dbe86e811322e77cfbaa8ea790e0af28a640f9efb5ba72afab13bbae9b5767d99e3d28a62ec02491bd"
+let seqPub4 = "0x9ecfdc9f63b7ff3f1771ef1003c2f74f381a3fc210f04f66a81eba56abd2e51a12c63c559c44233332b691eb733c26ad0aea8edd5afd4ba0b8b701988327a882"
+let seqAddr4 = "0x3EB630c3c267395fEE216b603A02061330d39642"
 
 const main = async () => {
   const accounts = await ethers.getSigners();
@@ -45,32 +43,33 @@ const main = async () => {
     await approveTx.wait();
   }
 
-  // lock params
-  // const sequencerSigner = signer;
-  // const pubkey = signerPubkey;
-
-   const sequencerSigner = addr4;
-   const pubkey = testPub4;
-
   const gov = await hre.ethers.getContractFactory("Proxy");
   const govProxyObj = await gov.attach(contractAddresses.contracts.GovProxy);
-  let setWitheAddressTx = await setWitheAddress(govProxyObj, contractAddresses.contracts.LockingPoolProxy, sequencerSigner);
-  await setWitheAddressTx.wait();
-  console.log("setWitheAddress:", setWitheAddressTx.hash);
-  
 
-  const lockAmount = web3.utils.toWei('2');
-  console.log(`Locking ${lockAmount} for ${sequencerSigner}...`);
-
-  console.log('locking now...')
-  let lockTx = await LockingPoolObj.lockFor(sequencerSigner, lockAmount, pubkey);
-  await lockTx.wait();
-  console.log("lock tx ", lockTx.hash);
+  await lockFor(govProxyObj, LockingPoolObj, contractAddresses, seqAddr1, seqPub1);
+  await lockFor(govProxyObj, LockingPoolObj, contractAddresses, seqAddr2, seqPub2);
+  await lockFor(govProxyObj, LockingPoolObj, contractAddresses, seqAddr3, seqPub3);
+  await lockFor(govProxyObj, LockingPoolObj, contractAddresses, seqAddr4, seqPub4);
 
   //  console.log('relocking now...')
   //  let reLockTx = await LockingPoolObj.relock(1, lockAmount, true);
   // await reLockTx.wait();
   //  console.log("reLock tx ", reLockTx.hash);
+}
+
+async function lockFor(govProxyObj, LockingPoolObj, contractAddresses, sequencerSigner, sequencerPubkey) {
+   let setWitheAddressTx = await setWitheAddress(govProxyObj, contractAddresses.contracts.LockingPoolProxy, sequencerSigner);
+   await setWitheAddressTx.wait();
+   console.log("setWitheAddress:", setWitheAddressTx.hash);
+
+
+   const lockAmount = web3.utils.toWei('20000');
+   console.log(`Locking ${lockAmount} for ${sequencerSigner}...`);
+
+   console.log('locking now...')
+   let lockTx = await LockingPoolObj.lockFor(sequencerSigner, lockAmount, sequencerPubkey);
+   await lockTx.wait();
+   console.log("lock tx ", lockTx.hash);
 }
 
 async function setWitheAddress(govObj, lockingPoolProxyAddress,user) {
