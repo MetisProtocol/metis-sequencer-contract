16f2798ea410033003a7c51a87d908261aaf870f
heipi <haiheipijuan@gmail.com>
2024-01-28 12:30:11 +0800

    add scripts

--- scripts/update_lock_limit.js
+++ scripts/update_lock_limit.js
@@ -2,8 +2,12 @@
      ethers
  } = require("hardhat");
 
+ const web3 = require("web3");
  const utils = require('./utils');
  let lockingPoolAddress;
+
+ const lockMax = "100000";
+ const lockMin = "20000";
 
  const main = async () => {
      const accounts = await ethers.getSigners();
@@ -12,17 +16,22 @@
 
      const contractAddresses = utils.getContractAddresses();
      console.log("contractAddresses:", contractAddresses);
-
      lockingPoolAddress = contractAddresses.contracts.LockingPoolProxy;
      console.log("lockingPoolAddress address:", lockingPoolAddress);
 
     const LockingPool = await ethers.getContractFactory("LockingPool");
     const LockingPoolObj = await LockingPool.attach(contractAddresses.contracts.LockingPoolProxy);
 
-    //  updateMpc
-    let updateMpcTx = await LockingPoolObj.updateMpc(sequencerSigner);
-    await updateMpcTx.wait();
-    console.log("updateMpc:", updateMpcTx.hash);
+
+    let lockMaxAmont = web3.utils.toWei(lockMax);
+    let updateLockMaxTx = await LockingPoolObj.updateMaxAmounts(lockMaxAmont);
+    await updateLockMaxTx.wait();
+    console.log("updateLockMax:", updateLockMaxTx.hash);
+
+    let lockMinAmont = web3.utils.toWei(lockMin);
+    let updateLockMinTx = await LockingPoolObj.updateMinAmounts(lockMinAmont);
+    await updateLockMinTx.wait();
+    console.log("updateLockMin:", updateLockMinTx.hash);
  }
 
  main()
