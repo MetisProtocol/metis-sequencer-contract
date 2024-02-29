67a521f5091b6ab20bdbf6159658f3774e3439e9
ericlee <eric.l@metis.io>
2024-02-29 16:43:22 +0800

    fix: remove erc721 imports

--- contracts/SequencerInfo.sol
+++ contracts/SequencerInfo.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
-import {ERC721EnumerableUpgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721EnumerableUpgradeable.sol";
 import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import {ISequencerInfo} from "./interfaces/ISequencerInfo.sol";
 
